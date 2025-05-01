6ba3aa00490c4cd21f3e795377620de6f2da51f6
Max Joseph <maxjoseph22@gmail.com>
2025-05-01 10:12:34 +0100

    added some dynamic variables

--- lib/hello.py
+++ lib/hello.py
@@ -10,5 +10,13 @@ def homepage_welcome():
 def leaderboard():
     return "<p>Welcome to the dogist leaderboard!</p>"
 
+@app.route("/dog")
+def dog():
+    return "<p>Welcome to the dog page!</p>"
+
+@app.route("/dog/<dog_id>")
+def individual_dog(dog_id):
+    return "Welcome to the specific dog page for dog " + str(dog_id)
+
 if __name__ == '__main__':
     app.run()
